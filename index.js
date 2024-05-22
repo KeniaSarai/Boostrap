2874d64e3c2610401f50bafecdf8ce194c861bd8
Sarai Flores Ortega <saraiflores1510@gmail.com>
2024-05-21 21:46:03 -0600

    Add files via upload
    Conocimos algunos códigos que Boostrap nos ofrece para facilitar la progrmación en Html

---
+++ index.js
@@ -0,0 +1,19 @@
+// ESCRIBIR NUESTRAS FUNCIONES 
+/* Función 1. generar un color aleatario */
+function generaColor(){
+/* Generar mi valor de RGB por separado y guardarlos
+en una variable */
+const rojo = F18B6C;
+const verde = ;
+const azul = ;
+}
+/* --> RGB(X,Y,Z) */
+
+/* Función 2. cambiar el color de fondo del cuerpo de la página */
+function cambioBackground(){
+    /* generaColor(); */
+}
+
+/* Buscar el boton en HTML por su clase */
+/* Cada vez que mi boton sea accionado, voy a ejecutar mi función 
+cambioBackground; */
